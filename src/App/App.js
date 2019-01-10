5d577e05e387e1e62761f3d98ffab70e1cfd033f
Michelle Beshears <meeshysu@gmail.com>
2019-01-09 18:37:03 -0600

    setup complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Auth from '../components/Auth/Auth';
 import './App.scss';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <button className='btn btn-danger'>HELP ME</button>
-          <Button
-            tag="a"
-            color="success"
-            size="large"
-            href="http://reactstrap.github.io"
-            target="_blank"
-          >
-            View Reactstrap Docs
-</Button>
-        </header>
+        <Auth />
       </div>
     );
   }
