90030c451e5c8f74868ee96bec59bbc95eabf5a9
Michelle Beshears <meeshysu@gmail.com>
2019-01-09 19:21:26 -0600

    logging in now works

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import authRequests from '../../helpers/data/authRequests';
 import './Auth.scss';
 
+import googleButton from '../../images/GoogleImage.png';
+
 class Auth extends React.Component {
+  static propTypes = {
+    isAuthenticated: PropTypes.func,
+  }
+
+  authenticateUser = (e) => {
+    e.preventDefault();
+    authRequests.authenticate()
+      .then(() => {
+        this.props.isAuthenticated();
+      })
+      .catch(err => console.error('there is an error with authenticate user', err));
+  }
+
   render() {
     return (
       <div className="Auth">
-        <button className="btn btn-danger">Login</button>
+        <button className="btn btn" onClick={this.authenticateUser}>
+          <img src={googleButton} alt="google login button" width="500px" />
+        </button>
       </div>
     );
   }
